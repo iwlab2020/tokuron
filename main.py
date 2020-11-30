a6750161fd97f3c12a606d60efdcb11c436d132b
tamichi <sp3h6ts9tamichi@hotmial.co.jp>
2020-12-01 06:52:53 +0900

    さいしゅうばん

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import random
 
 class Player():
     hand = []
-    state = 1 #1:同じ数字のみを出せる状態, 2:一つ大きい数字のみを出せる状態 3:2種類出せる状態, 4:何も出せない状態, 5:終了状態, 6:上がった状態
-    black_set = 0#=set単位のblack
-    white_set = 0#set単位のwhite
-    black_game = 0#game単位のblack
-    white_game = 0#game単位のwhite
+    state = 1      #1:同じ数字のみを出せる状態, 2:一つ大きい数字のみを出せる状態 3:2種類出せる状態, 4:何も出せない状態, 5:終了状態, 6:上がった状態
+    black_set = 0  #=set単位のblack
+    white_set = 0  #set単位のwhite
+    black_game = 0 #game単位のblack
+    white_game = 0 #game単位のwhite
 
 def eqplusChange(field):#eq,plusを算出する
     eq = field
@@ -109,137 +109,155 @@ def gameFinish(pl1,pl2):#gameの終了判定
 
 def canMove(s, a):
     if s >= 1 and s <= 6 and a >= 1 and a <= 4:
-        if s == 1 and a == 3:
-            return False
-        elif s == 2 and a == 2:
-            return False
-        elif (s == 4) and (a == 2 or a == 3):
-            return False
+        if a == 0:
+            if s == 4 or s == 5 or s == 6: return False
+            else: return True
+        if a == 1:
+            if s == 1: return True
+            else: return False
+        elif a == 2:
+            if s == 2: return True
+            else: return False
+        elif a == 3:
+            if s == 1 or s == 2 or s == 3 or s == 4: return True
+            else: return False
         else:
-            return True
-    else:
-        return False
-    
-def MinQ(Q, s):
-    min = Q[s, 0]
-    act = 0
-    for i in range(1, 4):
-        if (min > Q[s, i]) and canMove(s, i):
-            min = Q[s, i]
-            act = i
-    return act
+            return False
+    return False
 
 def eGreedy(Q, s, e):
     if random.random() < 1-e:
-        return MinQ(Q, pl.state)
+        #print("s = ", s)
+        min = Q[s][0]
+        act = 0
+        for i in range(4):
+            if canMove(s, i) and (min > Q[s][i]):
+                min = Q[s][i]
+                act = i
+        #print("act = ", act)
+        return act
     else:
-        return random.randint(0, 3)
+        return randhand(s)
+
+def randhand(s):
+    act = random.randint(0, 3)
+    while canMove(s, act) == False:
+        act = random.randint(0, 3)
+    return act
 
 # Action 0：降りる，1：fieldを出す，2：nextを出す，3：引く
-def doAction(pl, field, deck, act):
+def doAction(pl, deck, field, act):
+    #print('act = ', act)
+    #print('field = ', field)
+    #print('state = ', pl.state)
     next = (field + 1) % 4
     if act == 0:
         fold(pl)
     elif act == 1:
+        i_next = pl.hand.index(field)
+        field = pl.hand[i_next]
+        #pl.hand.pop(i_next)
         pl.hand.remove(field)
     elif act == 2:
         i_next = pl.hand.index(next)
         field = pl.hand[i_next]
-        pl.hand.remove(i_next)
+        #pl.hand.pop(i_next)
+        pl.hand.remove(field)
     elif act == 3:
-        if len(deck) == 0: fold(pl)
-
-def playCard(pl,lama_deck,field,eq,plus):#行動選択
-    deck = lama_deck#デッキ
-    fi = field#
-    x = 0#ループ用
-    while x < 1:#行動選択するまで終了しないために
-        # 0:降り, 1:出す, 2:引く
-        print(pl.hand)
-        act = int(input())#行動選択
-        if act == 0:#降り
+        if len(deck) == 0:
             fold(pl)
-            x = 1
-        elif act == 1:#出す
-            if pl.state == 4:
-                print("それはできません")
-            else:
-                print("カード")
-                print(pl.hand)
-                y = 0
-                while y < 1:#エラー処理
-                    put = int(input())#配列の位置を選択
-                    if (pl.hand[put] == eq) | (pl.hand[put] == plus):#出せるかどうか
-                        y = 1
-                    else:
-                        print("それは出せません")
-                fi = pl.hand[put]
-                pl.hand.pop(put)
-                x = 1
-        elif act == 2:#
-            if len(deck) == 0:#
-                print("それはできません")
-            else:#
-                x = deck.pop(0)
-                pl.hand.append(x)
-                pl.hand.sort()
-                x = 1
-    return deck, fi
+        else: 
+            x = deck.pop(0)
+            pl.hand.append(x)
+            pl.hand.sort()
+    
+    return deck, field
 
 #ココからメイン
 if __name__ == '__main__':
-    
+    Q = [[0 for j in range(4)] for i in range(7)] #Qtable
+    SumReward = [[0 for j in range(4)] for i in range(7)] #累積報酬テーブル
+    RewardCount = [[0 for j in range(4)] for i in range(7)] #報酬獲得回数テーブル
+    Rules = []
+    e = 0
     global lama_deck #デッキ
     field = 0 #場の数
     eq = 0 #stateを出すときに使う．fieldと同じ数
     plus = 0 #stateを出すときに使う．eqの一つ次の数
     lama_deck=[]
-    playcount = 1 #回すゲーム数
+    playcount = 1000000 #回すゲーム数
+    winrates = []
+    wincount = 0
     i = 0 #
     #下からゲームスタート
     while i < playcount:
         player1 = Player()
         player2 = Player()
         while(gameFinish(player1,player2)):
-            box = initialize(player1, player2,lama_deck,field)#初期化
+            box = initialize(player1, player2,lama_deck,field) #初期化
             lama_deck = box[0]
             field = box[1]
+            #setのループ
+            episode = 0
             while(isFinish(player1,player2)):
-                eqplus = eqplusChange(field)#eqとplusの値を算出
+                eqplus = eqplusChange(field) #eqとplusの値を算出
                 eq = eqplus[0]
                 plus = eqplus[1]
                 stateUpdate(player1,player2,eq,plus)
-                if player1.state < 5:#降りていないかの確認
-                    print("場の数" )
-                    print(field)
-                    print("先手")
-                    box = playCard(player1,lama_deck,field,eq,plus)
-                    lama_deck = box[0]
-                    field = box[1]
+                if player1.state < 5: #降りていないかの確認
+                    #print("場の数" )
+                    #print(field)
+                    #print("先手")
+                    #box = playCard(player1,lama_deck,field,eq,plus)
+                    act = randhand(player1.state)
+                    lama_deck, field = doAction(player1, lama_deck, field, act)
+                    #lama_deck = box[0]
+                    #field = box[1]
                 setpointUpdate(player1)
                 eqplus = eqplusChange(field)
                 eq = eqplus[0]
                 plus = eqplus[1]
                 stateUpdate(player1,player2,eq,plus)
-                if(isFinish(player1,player2)):#先手側が上がっていた時にプレイできないようにするため
-                    if player2.state < 5:#降りていないかの確認
-                        print("場の数" )
-                        print(field)
-                        print("後手")
-                        box = playCard(player2,lama_deck,field,eq,plus)
-                        lama_deck = box[0]
-                        field = box[1]
+                if(isFinish(player1,player2)): #先手側が上がっていた時にプレイできないようにするため
+                    s = player2.state
+                    if s < 5: #降りていないかの確認
+                        #print("場の数" )
+                        #print(field)
+                        #print("後手")
+                        #box = playCard(player2,lama_deck,field,eq,plus)
+                        act = eGreedy(Q, s, e)
+                        lama_deck, field = doAction(player2, lama_deck, field, act)
+                        Rules.append([s, act])
+                        #lama_deck = box[0]
+                        #field = box[1]
+                        stateUpdate(player1, player2, field, (field+1) % 4)
+                        episode += 1
                 setpointUpdate(player2)
+            # セットポイントの計算
+            point1 = player1.black_set * 5 + player1.white_set
+            point2 = player2.black_set * 5 + player2.white_set
+            # Player2が負けなら報酬
+            if point1 > point2:
+                for t in range(episode):
+                    RewardCount[Rules[t][0]][Rules[t][1]] += 1
+                    SumReward[Rules[t][0]][Rules[t][1]] -= point2
+                    Q[Rules[t][0]][Rules[t][1]] = SumReward[Rules[t][0]][Rules[t][1]] / RewardCount[Rules[t][0]][Rules[t][1]]
             gamepointUpdate(player1)
             gamepointUpdate(player2)
-            print(player1.hand)
+            #print(player1.hand)
             if len(player1.hand) == 0:
                 pointMinus(player1)#上がっていたら一枚減らす
             if len(player2.hand) == 0:
                 pointMinus(player2)#上がっていたら一枚減らす
-            print("先手のポイント")
-            print(player1.black_game * 5 + player1.white_game)
-            print("後手のポイント")
-            print(player2.black_game * 5 + player2.white_game)
+
+        if (player1.black_game * 5 + player1.white_game) > (player2.black_game * 5 + player2.white_game):
+            wincount += 1
         i += 1
+        winrates.append((wincount/i)*100)
+
+    
+    with open('Monte_Carlo.csv', 'w') as w_file:
+        for i in range(playcount):
+            w_file.write(str(winrates[i]) + ',\n')
+        
 
